f8263ecc8c75d508a31bee964939725f3bf685dc
adajett <adajett@gmail.com>
2019-03-25 13:40:35 -0700

    Merge pull request #3 from kayaktavist/abel-branch
    fixed issues

--- client/public/src/components/pages/404page/noMatch.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const noMatch = props => (
-    <div>
-        <h1>404 error, the gnome has led you astray. This page does not exist.</h1>
-    </div>
-)
-
-export default noMatch;
--- client/public/src/components/pages/Discover/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Discover = props => (
-    <div>
-        <h1>This is the Discover Page</h1>
-    </div>
-)
-
-export default Discover;
