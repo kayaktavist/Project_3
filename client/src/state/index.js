4e4335cf6dd8e211c96388e9c72d52c5afb207e5
Alex <alex.j.biggers@gmail.com>
2019-04-01 13:31:53 -0700

    stuff

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -15,9 +15,9 @@ const rootReducer = combineReducers({
 const store = createStore(
   rootReducer,
   
-  compose(  applyMiddleware(thunk)),
+  compose(  applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  
+  )
 );
 
 export default store;
